1196b435e71437cef5dee3ab8772c77890161632
fp-torres <felipetorresaraujo@gmail.com>
2026-09-16 17:38:13 -0300

    feat: Adicionar exercícios de área e preço do terreno ao código

--- src/Main.java
+++ src/Main.java
@@ -44,6 +44,7 @@ public class Main {
         // PRODUTOS
         // ==============================
 
+        /*
         String product1 = "Computer";
         String product2 = "Office desk";
 
@@ -112,5 +113,35 @@ public class Main {
             "US decimal point: %.3f%n",
             measure
         );
+        */
+
+
+        // ==============================
+        // EXERCÍCIO - ÁREA E PREÇO DO TERRENO
+        // ==============================
+
+        /*
+        
+        Fazer um programa para ler as medidas da largura e comprimento
+        de um terreno retangular com uma casa decimal, bem como o valor
+        do metro quadrado do terreno com duas casas decimais.
+
+        Em seguida, o programa deve mostrar o valor da área do terreno,
+        bem como o valor do preço do terreno, ambos com duas casas decimais.
+
+        Exemplo:
+
+        Entrada:
+        10.0
+        30.0
+        200.00
+
+        Saída:
+        AREA = 300.00
+        PRECO = 60000.00
+
+        */
+
+
     }
 }
