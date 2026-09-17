735dfcff331021525acd5a7ed3e3885ef1f8eca5
fp-torres <felipetorresaraujo@gmail.com>
2026-09-17 11:11:53 -0300

    feat: Adicionar cálculo de área e preço com entrada do usuário, exercicio resolvido

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,5 @@
 import java.util.Locale;
-
+import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
@@ -141,6 +141,22 @@ public class Main {
         PRECO = 60000.00
 
         */
+        Locale.setDefault(Locale.US);
+
+        Scanner sc = new Scanner(System.in);
+
+        double largura = sc.nextDouble();
+        double comprimento = sc.nextDouble();
+        double metroQuadrado  = sc.nextDouble();
+
+        double area = largura * comprimento;
+        double preco = area * metroQuadrado;
+
+        System.out.printf("AREA = %.2f%n", + area);
+        System.out.printf("PRECO = %.2f%n", + preco);
+
+        sc.close();
+        
 
 
     }
