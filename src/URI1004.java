3c4e7304563a7ccdfbc3ad5eaab890e3b73d56d3
fp-torres <felipetorresaraujo@gmail.com>
2026-09-23 17:32:58 -0300

    feat: Adicionar solução para o exercício URI 1004 - cálculo do produto de dois números

---
+++ src/URI1004.java
@@ -0,0 +1,26 @@
+import java.util.Locale;
+import java.util.Scanner;
+
+
+public class URI1004 {
+
+    public static void main(String[] args) {
+
+        Locale.setDefault(Locale.US);
+                
+        System.out.println("URI 1004");
+                              
+        Scanner sc = new Scanner(System.in);
+
+        int A = sc.nextInt();
+
+        int B = sc.nextInt();
+
+        int PROD = A * B;
+
+        System.out.println("PROD = " + PROD);
+
+        sc.close();       
+
+    }
+}
