4d49cb87221535855697f5396f3facca709aecf3
fp-torres <felipetorresaraujo@gmail.com>
2026-09-25 17:46:50 -0300

    feat: Adicionar solução para o exercício URI 1010 - cálculo do salário com base em produtos, em andamento

---
+++ src/URI1010.java
@@ -0,0 +1,32 @@
+import java.util.Locale;
+import java.util.Scanner;
+
+
+public class URI1010 {
+
+    public static void main(String[] args) {
+
+        Locale.setDefault(Locale.US);
+                
+        System.out.println("URI 1010");
+                              
+        Scanner sc = new Scanner(System.in);
+
+       int code1 = sc.nextInt();
+       int quantity1 = sc.nextInt();
+       double price1 = sc.nextDouble();
+
+       int code2 = sc.nextInt();
+       int quantity2 = sc.nextInt();
+       double price2 = sc.nextDouble();
+       
+
+        System.out.println("NUMBER = " + number);
+        System.out.printf("SALARY = U$ %.2f%n ", salary);
+
+        
+
+        sc.close();       
+
+    }
+}
